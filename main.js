d7ca29415aee81cebc4734eca035d102c6c006a2
Dankore <zimmazone@yahoo.com>
2019-05-27 10:06:52 +0000

    add shark api

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+//Unicrons
 const pressed = [];
 const secretCode = 'a';
 
-
-window.addEventListener('keyup', (e) =>{
+window.addEventListener('keyup', (e) =>  {
     pressed.push(e.key)
     
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
@@ -14,3 +14,23 @@ window.addEventListener('keyup', (e) =>{
     
     console.log(pressed);
 })
+
+//Sharks
+const sharks = [];
+const secret = 'b';
+
+window.addEventListener('keyup', (e) =>  {
+    sharks.push(e.key)
+    
+    sharks.splice(-secret.length - 1, sharks.length - secret.length);
+    
+    if(sharks.join('').includes(secret)){
+        console.log('YES TO SHARKS!');
+        sharkify_add();
+    };
+    
+    console.log(sharks);
+})
+
+
+
